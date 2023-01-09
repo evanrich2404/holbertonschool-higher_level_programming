6b5fc8188e29705f5db7aef7e6173304af35da85
evanrich2404 <evanrich2404@gmail.com>
2023-01-09 18:04:37 +0000

    I got it working, lets hope it is PEP8

---
+++ 0x00-python-hello_world/3-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+# Last digit of number is last_digit and is less than 6 and not 0
+# Last digit of number is last_digit and is 0
+# Last digit of number is last_digit and is greater than 5
+if number < 0:
+    last_digit = number % -10
+else:
+    last_digit = number % 10
+
+if last_digit != 0 and last_digit < 6:
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0")
+elif last_digit == 0:
+    print("Last digit of", number, "is", last_digit, "and is 0")
+else:
+    print("Last digit of", number, "is", last_digit, "and is greater than 5")
