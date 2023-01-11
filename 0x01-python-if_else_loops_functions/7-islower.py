a001409fb9d685545a16aafc7bc60bfe8eaf4dcd
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 05:41:46 +0000

    testing stuff

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for asci in range(ord('a'), ord('z'), ord('A'), ord('Z')):
-        if c[asci] == (ord('a'), ord('z')):
-            return True
-        else:
-            return False
+    lower = [range(ord('a'), ('z'))]
+    if c == lower:   
+        return True
+    else:
+        return False
