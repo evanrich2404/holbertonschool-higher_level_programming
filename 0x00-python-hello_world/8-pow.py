8a554febaad8b50f859fda689a7aaac8d3c108c4
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 23:43:24 +0000

    maybe -1 will help

--- 0x00-python-hello_world/8-pow.py
+++ 0x00-python-hello_world/8-pow.py
@@ -2,13 +2,7 @@
 def pow(a, b):
     if b == 0:
         return 1
-    elif b == 1:
-        return a
-    elif b % 2 == 0:
-        return pow(a * a, b / 2)
-    elif b % 2 == 1:
-        return a * pow(a * a, (b - 1) / 2)
     elif b < 0:
-        return 1 / pow(a, -b)
+        return a * pow((a,b) * -1)
     else:
         return a * pow(a, b - 1)
