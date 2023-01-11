66d686a0968b7f4f0c4538a3a665efb8afd26f59
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 23:40:59 +0000

    is this too much

--- 0x00-python-hello_world/8-pow.py
+++ 0x00-python-hello_world/8-pow.py
@@ -2,5 +2,13 @@
 def pow(a, b):
     if b == 0:
         return 1
+    elif b == 1:
+        return a
+    elif b % 2 == 0:
+        return pow(a * a, b / 2)
+    elif b % 2 == 1:
+        return a * pow(a * a, (b - 1) / 2)
+    elif b < 0:
+        return 1 / pow(a, -b)
     else:
         return a * pow(a, b - 1)
