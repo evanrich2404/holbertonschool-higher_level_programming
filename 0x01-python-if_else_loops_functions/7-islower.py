7c568bf6c3171f0da51e965ac8972b3908ad290a
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 20:57:12 +0000

    tests with elif

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,14 @@
 def islower(c):
     for char in c:
         k = char.islower()
-        if (k == True):
+        if k == True:
             return True
         else:
             return False
+
+
+   # lower = [range(ord('a'), ord('z'))]
+    #if c == lower:   
+     #   return True
+   # else:
+    #    return False
