409861541cc1bd4cdb1f101e3d298004cfa395e3
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 20:42:20 +0000

    I got it working and understand the logic, added comments

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-def comb(L):
-    for num1 in range(0, 10):
-        for num2 in range(1, 10):
-            if L in ("num1, num2" != "num2, num1"):
-                print(L[num1] + L[num2])
-
-
-
-#!/usr/bin/python3
-for asci in range(ord('a'), ord('z')+1):
-    notasci = chr(asci)
-    if notasci not in [('e'), ('q')]:
-        print(notasci, end="")
-        [".format("]
+for num1 in range(0, 10):
+    for num2 in range(1, 10):
+        # this makes sure the numbers don't repeat
+        if (num1 >= num2):
+            continue
+        # this makes sure it ends at 89
+        elif (num1 == 8 and num2 == 9):
+            print("{}{}".format(num1, num2))
+        # this makes sure it prints the numbers, and in the correct format
+        else:
+            print("{}{}".format(num1, num2), end=", ")
