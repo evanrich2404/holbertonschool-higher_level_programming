f9e8d30641254eadf6230efcf55bb87cfd3a256c
evanrich2404 <evanrich2404@gmail.com>
2023-01-11 05:50:29 +0000

    pushing all

---
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def comb(L):
+    for num1 in range(0, 10):
+        for num2 in range(1, 10):
+            if L in ("num1, num2" != "num2, num1"):
+                print(L[num1] + L[num2])
+
+
+
+#!/usr/bin/python3
+for asci in range(ord('a'), ord('z')+1):
+    notasci = chr(asci)
+    if notasci not in [('e'), ('q')]:
+        print(notasci, end="")
+        [".format("]
