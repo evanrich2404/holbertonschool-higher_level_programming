f641c38c93e350897b338a11ada75067c1044c53
evanrich2404 <evanrich2404@gmail.com>
2023-01-12 00:50:44 +0000

    changed some logic around

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-length = len(sys.argv)
-if length == 1:
-    print("{:d} argument.".format(length - 1))
-elif length == 2:
-    print("{:d} argument:".format(length - 1))
-else:
-    print("{:d} argument:".format(length - 1))
-for i in range(1, length):
-    print("{:d}: {}".format(i, sys.argv[i]))
+    length = len(sys.argv) - 1
+    if length < 1:
+        print("{} argument.".format(length - 1))
+    elif length == 1:
+        print("{} argument:".format(length - 1))
+    else:
+        print("{} argument:".format(length - 1))
+    for i in range(1, length + 1):
+        print("{}: {}".format(i, sys.argv[i]))
