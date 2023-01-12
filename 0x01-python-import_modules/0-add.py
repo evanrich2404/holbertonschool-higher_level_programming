ace5dd75f374989244e009e42227005e966dbb9e
evanrich2404 <evanrich2404@gmail.com>
2023-01-12 00:22:39 +0000

    does this work

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import def add(a, b) from add_0.py:
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
-["__main__"] ["__name__"]
+    return a + b
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+else:
+    print("Imported")
