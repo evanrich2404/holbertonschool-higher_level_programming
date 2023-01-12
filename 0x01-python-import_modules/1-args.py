b86e92c83f5495bc993fbd7c22004e97826ba27b
evanrich2404 <evanrich2404@gmail.com>
2023-01-12 22:10:44 +0000

    nothing works, it can't find my files idkwhy

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv) - 1
-    if length < 1:
-        print("{} argument.".format(length - 1))
-    elif length == 1:
-        print("{} argument:".format(length - 1))
+    count = len(sys.argv) - 1
+    if count < 1:
+        print("{} arguments.".format(count))
+    elif count == 1:
+        print("{} argument:".format(count))
     else:
-        print("{} argument:".format(length - 1))
-    for i in range(1, length + 1):
+        print("{} arguments:".format(count))
+    for i in range(1, count + 1):
         print("{}: {}".format(i, sys.argv[i]))
