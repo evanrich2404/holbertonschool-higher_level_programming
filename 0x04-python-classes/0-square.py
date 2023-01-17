19039e9dfc13b9222840a5e00793004aaed4c2e6
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 06:15:39 +0000

    I forgot to add documentation

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
+        """Initializing the size"""
         self.__size = size
     
     def get_size(self):
+        """And now getting the size"""
         return self.__size
