0996d88ddf3ffb2aa9224ac62e397d84e2730fe3
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 06:24:10 +0000

    Okay I am figuring it out via repititon

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,10 +2,9 @@
 """I am making a class for Squares, and I am going to make a getter for size.
 Hopefully this works."""
 class Square:
+    """This is a class for squares. Which should also get teh size of the square. Size is a private attribute. Maybe this will please the checker."""
     def __init__(self, size):
-        """Initializing the size"""
         self.__size = size
     
     def get_size(self):
-        """And now getting the size"""
         return self.__size
