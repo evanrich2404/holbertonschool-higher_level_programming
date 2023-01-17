991afa6b37abf6bf7a7b9ecdc531081c9cd948ad
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 06:41:03 +0000

    I made a ruler for 79th char. Now I gotta find out why it wants blank lines

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,9 +2,8 @@
 """I am making a class for Squares, and I am going to make a getter for size.
 Hopefully this works."""
 class Square:
-
-
-    """This is a class for squares. Which should also get teh size of the square. Size is a private attribute."""
+"""This is a class for squares. Which should also get teh size of the square.
+Size is a private attribute."""
     def __init__(self, size):
         self.__size = size
 
