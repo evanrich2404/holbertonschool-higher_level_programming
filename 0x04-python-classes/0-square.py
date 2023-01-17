8fad1cd557d867056235f0ef9998e645641d382e
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 06:25:56 +0000

    okay pep8 time yuh

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,9 +2,11 @@
 """I am making a class for Squares, and I am going to make a getter for size.
 Hopefully this works."""
 class Square:
-    """This is a class for squares. Which should also get teh size of the square. Size is a private attribute. Maybe this will please the checker."""
+
+
+    """This is a class for squares. Which should also get teh size of the square. Size is a private attribute."""
     def __init__(self, size):
         self.__size = size
-    
+
     def get_size(self):
         return self.__size
