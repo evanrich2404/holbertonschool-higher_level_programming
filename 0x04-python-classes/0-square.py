e29d403929c6c9ea29bec4449e898b1c6409a501
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 06:49:50 +0000

    Hopefully this is beautiful and the checker will love me

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-"""I am making a class for Squares, and I am going to make a getter for size.
-Hopefully this works."""
+"""
+This is a module for a class Square
+which contains a private attribute size.
+"""
 class Square:
-"""This is a class for squares. Which should also get teh size of the square.
-Size is a private attribute."""
+"""
+This is a class Square with a private attribute size.
+I am not sure how to be more specific.
+Trying to please the PEP8 gods.
+"""
     def __init__(self, size):
         self.__size = size
 
