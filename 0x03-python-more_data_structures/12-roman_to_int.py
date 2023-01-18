240d7c278c1f44262cee980e8d338e253bce64c4
evanrich2404 <evanrich2404@gmail.com>
2023-01-17 16:12:02 -0800

    forgot to check for str or none

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+def roman_to_int(roman_strings):
+    roman_dict = {'I': 1, 'V': 5, 'X': 10,
+     'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_list = list(roman_strings)
+    roman_list.reverse()
+    result = 0
+    if roman_strings == '' or roman_strings == None or roman_strings != str:
+        return 0
+    else:
+        for i in range(len(roman_list)):
+            if i == 0:
+                result += roman_dict[roman_list[i]]
+            elif roman_dict[roman_list[i]] < roman_dict[roman_list[i - 1]]:
+                result -= roman_dict[roman_list[i]]
+            else:
+                result += roman_dict[roman_list[i]]
+    return result
