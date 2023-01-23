db44f3efb3332402cfb32508fb83e981c38edc20
evanrich2404 <evanrich2404@gmail.com>
2023-01-23 08:48:15 +0000

    PEP8 time

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,42 +7,54 @@ Private instance attribute: height
 
 
 class Rectangle:
-    
+
     """Class Rectangle that defines a rectangle
     In this class we define a rectangle with private instance attributes
     We use property and setter to validate the data
     """
-    
+
     def __init__(self, width=0, height=0):
-        
+
         """
         Instantiation with optional width and height
         Args:
             width (int): width of a rectangle
             height (int): height of a rectangle
         """
-        
+
         self.width = width
         self.height = height
+
     @property
     def width(self):
+
         """Property to retrieve width"""
+
         return self.__width
+
     @width.setter
     def width(self, value):
+
         """Property setter to set width"""
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
+
         """Property to retrieve height"""
+
         return self.__height
+
     @height.setter
     def height(self, value):
+
         """Property setter to set height"""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
