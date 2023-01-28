d70dd8175b9b5b4423a732063e2a6dbb6427ce9d
evanrich2404 <evanrich2404@gmail.com>
2023-01-28 14:14:48 -0800

    checking the type of the obj

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    """Checks if the object is exactly an instance of the specified class"""
+    return type(obj) is a_class
