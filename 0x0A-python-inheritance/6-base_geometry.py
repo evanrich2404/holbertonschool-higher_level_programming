4052ccbe1bd15ea112d32bb6187ceab67ed3d777
evanrich2404 <evanrich2404@gmail.com>
2023-01-28 14:47:35 -0800

    exception time

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module contains a class BaseGeometry"""
+
+
+class BaseGeometry:
+    """This class defines a base geometry"""
+    def area(self):
+        """This method raises an exception"""
+        raise Exception("area() is not implemented")
