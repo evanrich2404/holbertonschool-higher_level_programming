000122838c29fd118c8f16f382d2af345c6b2f8b
evanrich2404 <evanrich2404@gmail.com>
2023-01-28 16:00:28 -0800

    square2 electric boogaloo

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" This module contains a function that creates a square class that
+inherits from Rectangle class
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ This class inherits from Rectangle class
+    """
+    def __init__(self, size):
+        """ This method initializes the class
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ This method returns the area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ This method returns a string representation of the square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
