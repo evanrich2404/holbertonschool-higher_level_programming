8bc73630dcc6ab874f5b7a355e59939b1c91e840
evanrich2404 <evanrich2404@gmail.com>
2023-01-30 21:48:49 -0800

    was not taking the right path to get what I wanted

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,18 +6,17 @@ It uses save_to_json_file and load_from_json_file
 list must be saved as a JSON representation in a file named add_item.json
 if the file doesn’t exist, it should be created
 """
-__import__('5-save_to_json_file').save_to_json_file()
-__import__('6-load_from_json_file').load_from_json_file()
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file()
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file()
 
 
-def add_item():
-    """ adds all arguments to a Python list, and then save them to a file """
-    import sys
-    import os.path
-    if os.path.exists('add_item.json'):
-        my_list = load_from_json_file('add_item.json')
-    else:
+if __name__ == "__main__":
+    """rocking the main"""
+    try:
+        my_list = load_from_json_file("add_item.json")
+    except:
         my_list = []
     for i in range(1, len(sys.argv)):
         my_list.append(sys.argv[i])
-    save_to_json_file(my_list, 'add_item.json')
+    save_to_json_file(my_list, "add_item.json")
