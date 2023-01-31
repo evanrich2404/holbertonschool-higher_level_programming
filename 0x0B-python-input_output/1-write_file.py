a933086180dac74a5a21e2f6677f712ba07c1927
evanrich2404 <evanrich2404@gmail.com>
2023-01-30 21:18:44 -0800

    write the string to file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+module to write a string to a text file (UTF8)
+and return the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """writes a string to a text file (UTF8) and returns the number of
+    characters written"""
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
