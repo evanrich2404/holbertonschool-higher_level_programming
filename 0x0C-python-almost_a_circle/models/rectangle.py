9bfb69e5a8875033a6aa926cc8a8d140f33334c0
evanrich2404 <evanrich2404@gmail.com>
2023-01-30 23:55:01 -0800

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+base = __import__('base').Base
+
+
+class Rectangle(base):
+    """Rectangle class"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """area"""
+        return self.__width * self.__height
+
+    def display(self):
+        """display"""
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for j in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """str"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y
