13b987dfc21a905d59d231e763ae4a2346f11cd0
evanrich2404 <evanrich2404@gmail.com>
2023-02-01 13:14:59 -0800

    Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,7 @@ base = __import__('base').Base
 
 
 class Rectangle(base):
-    """Rectangle class"""
+"""Rectangle class"""
     def __init__(self, width, height, x=0, y=0, id=None):
         """init"""
         super().__init__(id)
@@ -12,12 +12,12 @@ class Rectangle(base):
         self.height = height
         self.x = x
         self.y = y
-
+    
     @property
     def width(self):
         """width"""
         return self.__width
-
+    
     @width.setter
     def width(self, value):
         """width"""
@@ -26,12 +26,12 @@ class Rectangle(base):
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-
+    
     @property
     def height(self):
         """height"""
         return self.__height
-
+    
     @height.setter
     def height(self, value):
         """height"""
@@ -40,12 +40,12 @@ class Rectangle(base):
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
-
+    
     @property
     def x(self):
         """x"""
         return self.__x
-
+    
     @x.setter
     def x(self, value):
         """x"""
@@ -59,7 +59,7 @@ class Rectangle(base):
     def y(self):
         """y"""
         return self.__y
-
+    
     @y.setter
     def y(self, value):
         """y"""
@@ -71,20 +71,52 @@ class Rectangle(base):
 
     def area(self):
         """area"""
-        return self.__width * self.__height
-
+        return self.width * self.height
+    
     def display(self):
         """display"""
-        for i in range(self.__y):
+        for i in range(self.y):
             print()
-        for i in range(self.__height):
-            for j in range(self.__x):
+        for i in range(self.height):
+            for j in range(self.x):
                 print(" ", end="")
-            for j in range(self.__width):
+            for j in range(self.width):
                 print("#", end="")
             print()
-
+    
     def __str__(self):
         """str"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.__x, self.__y
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
+            self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        """update"""
+        if args:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[i]
+                elif i == 1:
+                    self.width = args[i]
+                elif i == 2:
+                    self.height = args[i]
+                elif i == 3:
+                    self.x = args[i]
+                elif i == 4:
+                    self.y = args[i]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        """to_dictionary"""
+        return {'id': self.id, 'width': self.width, 'height': self.height,
+                'x': self.x, 'y': self.y}
