50f07ad4aab66808034a379b95db7f1a951dc96c
evanrich2404 <evanrich2404@gmail.com>
2023-02-13 14:47:14 -0600

    got the syntax reshaped

--- 0x01-SQL_more_queries/7-cities.sql
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -1,10 +1,10 @@
 -- create the database hbtn_0d_usa and table cities
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities(
+CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     PRIMARY KEY(id),
     UNIQUE (id),
     FOREIGN KEY (state_id) REFERENCES states(id)
