b319d6a85d834d52c2b5c01e290a1f06a5cbb86b
evanrich2404 <evanrich2404@gmail.com>
2023-02-25 17:42:35 -0600

    filter it by n

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""1-filter_states.py"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """1-filter_states.py"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
