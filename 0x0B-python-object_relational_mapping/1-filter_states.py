660a7519da38dd12ba5f020788d1d348e486a6fd
evanrich2404 <evanrich2404@gmail.com>
2023-02-25 17:44:13 -0600

    trying a different print

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -6,12 +6,9 @@ import sys
 
 if __name__ == "__main__":
     """1-filter_states.py"""
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    [print(state) for state in cur.fetchall() if state[1][0] == 'N']
     cur.close()
     db.close()
