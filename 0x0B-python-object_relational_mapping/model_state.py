02e21963388ff81c38824274a3ab617157f7d63f
evanrich2404 <evanrich2404@gmail.com>
2023-02-28 21:31:06 -0600

    removed id not being able to be nullable

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -8,6 +8,6 @@ Base = declarative_base()
 class City(Base):
     """The City Class"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
