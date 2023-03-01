fcfcead42f9a41bb74b7fb3aae4c7c7b2a4c29a5
evanrich2404 <evanrich2404@gmail.com>
2023-02-28 21:43:01 -0600

    literally how did I start doing city

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""The City Class Module"""
+"""The State Class Module"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class City(Base):
-    """The City Class"""
-    __tablename__ = 'cities'
+class State(Base):
+    """The State Class"""
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
