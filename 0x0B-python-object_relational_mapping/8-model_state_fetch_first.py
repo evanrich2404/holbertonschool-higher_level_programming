8f9d2691c6bfe899ee511ffc40db463e6ff179ba
evanrich2404 <evanrich2404@gmail.com>
2023-02-28 21:58:16 -0600

    was missing a check

--- 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0B-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,21 +6,14 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 
-def fetch_all_states():
-    """fetches all states"""
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                             .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
-
     state = session.query(State).order_by(State.id).first()
     if state:
         print("{}: {}".format(state.id, state.name))
     else:
         print("Nothing")
-
-
-if __name__ == "__main__":
-    fetch_all_states()
