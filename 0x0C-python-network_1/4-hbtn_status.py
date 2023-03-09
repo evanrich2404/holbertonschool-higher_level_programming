f77687269c6058ce128044e7bde25099aae08e04
evanrich2404 <evanrich2404@gmail.com>
2023-03-09 17:49:52 -0600

    oh it was using a different module

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """fetching the status again"""
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
