a36c1912e8adfd59929db02b48e1a67a909647d6
evanrich2404 <evanrich2404@gmail.com>
2023-03-12 21:01:19 -0500

    pycodestyle

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -9,8 +9,8 @@ import sys
 
 if __name__ == "__main__":
     r = requests.get('https://api.github.com/user', auth=(sys.argv[1],
-                                                           sys.argv[2]))
+                                                          sys.argv[2]))
     try:
         print(r.json().get('id'))
-    except:
+    except Exception:
         print("None")
