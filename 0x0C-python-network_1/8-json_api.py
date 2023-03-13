30b480cbcd6be816ad4fb8ccff3d64a91d8202b7
evanrich2404 <evanrich2404@gmail.com>
2023-03-12 20:46:43 -0500

    json api test

---
+++ 0x0C-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+takes in a letter and sends POST request to 
+http://0.0.0.0:5000/search_user with letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+
+    if r.headers.get('content-type') != 'application/json':
+        print("Not a valid JSON")
+    elif r.json() == {}:  # if r.json() is empty
+        print("No result")
+    else:
+        print("[{}] {}".format(r.json().get('id'), r.json().get('name')))
