a03d90ff03754f15b756c352c92dcd11fadc3b42
evanrich2404 <evanrich2404@gmail.com>
2023-04-08 20:42:19 -0500

    prints them all on new lines

---
+++ 0x00-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// prints “C is fun”, “Python is cool”, “Javascript is amazing”
+const myC = 'C is fun';
+const myP = 'Python is cool';
+const myJS = 'Javascript is amazing';
+console.log(`${myC}
+${myP}
+${myJS}`);
