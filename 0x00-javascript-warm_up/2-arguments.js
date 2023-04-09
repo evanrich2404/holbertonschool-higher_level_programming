906f235052753e6dcc7fe45b1c74b58ada3744a2
evanrich2404 <evanrich2404@gmail.com>
2023-04-08 21:02:06 -0500

    I thought I pushed this

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-// prints the number of arguments already passed 
-const myArgs = process.argv;
-console.log(myArgs.length - 2);
+// prints a message depending of the number of arguments passed
+const myArgs = process.argv.slice(2);
+if (myArgs.length === 0) {
+  console.log('No argument');
+} else if (myArgs.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
