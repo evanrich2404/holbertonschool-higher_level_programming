155aa20c6e998fccb78588735fa69f7e3e9572d0
evanrich2404 <evanrich2404@gmail.com>
2023-04-19 11:16:03 -0500

    found the bug for task 9 and changed it back

--- 0x03-javascript-web_jquery/9-script.js
+++ 0x03-javascript-web_jquery/9-script.js
@@ -1,7 +1,6 @@
 // fetches from https://stefanbohacek.com/hellosalut/?lang=fr and displays the value of hello from that fetch in the HTML’s tag DIV#hello
 $(document).ready(function() {
-  $.get('https://stefanbohacek.com/hellosalut/?lang=fr', function(data) {
-    const helloTranslation = $(data).find('#hello').text();
-    $('#hello').text(helloTranslation);
+  $.get('https://hellosalut.stefanbohacek.dev/?lang=fr', function(data) {
+    $('#hello').text(data.hello);
   });
 });
